565d64e2009ecbabaab16b03b92418b08698ceba
Liner Yang <lineryang@gmail.com>
2016-12-25 16:31:21 +0800

    segmantation scripts

--- chinese/segmentation/scripts/run.sh
+++ chinese/segmentation/scripts/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-for i in `find ../../extract-text/data/ -name *.txt`;
-do
-  echo $i
-  # echo ../data/`basename $i .txt`.seg
-  ./stanford-segmenter-2015-12-09/segment.sh ctb  $i UTF-8 0 > ../data/`basename $i .txt`.seg
+cd stanford-segmenter-2015-12-09/
+javac -cp stanford-segmenter-3.6.0.jar SegGigaword.java
+
+start=$1
+end=$2
+
+for i in `seq  -f %03g $start $end`  
+do  
+    # echo $i 
+    mkdir ../../split/$i
+    java -mx2g -cp "*:." SegGigaword ../../../extract-text/split/$i ../../split/$i
 done
-
-
-
---
+++ chinese/segmentation/scripts/stanford-segmenter-2015-12-09/SegGigaword.java
@@ -0,0 +1,87 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ling.CoreLabel;
+
+/**
+ * This is a very simple demo of calling the Chinese Word Segmenter
+ * programmatically. It assumes an input file in UTF8.
+ * <p/>
+ * <code>
+ *  Usage: java -mx1g -cp seg.jar SegDemo fileName
+ *  </code> This will run correctly in the distribution home directory. To run
+ * in general, the properties for where to find dictionaries or normalizations
+ * have to be set.
+ *
+ * @author Christopher Manning
+ */
+
+public class SegGigaword {
+
+	private static final String basedir = System.getProperty("SegGigaword", "data");
+
+	private List<String> getDirectory(File file) {
+		File flist[] = file.listFiles();
+		if (flist == null || flist.length == 0) {
+			return null;
+		}
+		List<String> file_names = new ArrayList<String>();
+		for (File f : flist) {
+			file_names.add(f.getName());
+		}
+		return file_names;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// System.setOut(new PrintStream(System.out, true, "utf-8"));
+
+		Properties props = new Properties();
+		props.setProperty("sighanCorporaDict", basedir);
+		// props.setProperty("NormalizationTable", "data/norm.simp.utf8");
+		// props.setProperty("normTableEncoding", "UTF-8");
+		// below is needed because CTBSegDocumentIteratorFactory accesses it
+		props.setProperty("serDictionary", basedir + "/dict-chris6.ser.gz");
+		props.setProperty("inputEncoding", "UTF-8");
+		props.setProperty("sighanPostProcessing", "true");
+
+		CRFClassifier<CoreLabel> segmenter = new CRFClassifier<>(props);
+		segmenter.loadClassifierNoExceptions(basedir + "/ctb.gz", props);
+		File inputDir = new File(args[0]);
+		File outputDir = new File(args[1]);
+		SegGigaword s = new SegGigaword();
+		List<String> file_list = s.getDirectory(inputDir);
+
+		for (String file_name : file_list) {
+			String input_file_name = inputDir.getAbsolutePath() + "/" + file_name;
+            int pos = file_name.indexOf(".");
+            String output_file_name = outputDir.getAbsolutePath() + "/" + file_name.substring(0,pos) + ".seg";
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input_file_name),"UTF-8"));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output_file_name),"UTF-8"));
+            String sentence = null;
+            System.out.println("seg file :" + input_file_name);
+            System.out.println("write to :" + output_file_name);
+			List<List<String>> result = new ArrayList<List<String>>();
+            while ((sentence = reader.readLine()) != null) {  
+            	List<String> words = segmenter.segmentString(sentence);
+				result.add(words);
+            }  
+			for(List<String> sent: result)
+			{
+				for(String word: sent)
+					writer.write(word + " ");
+				writer.newLine();
+			}
+			reader.close();
+			writer.close();
+		}
+	}
+}
